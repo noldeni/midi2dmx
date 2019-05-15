0f8b26bfaa941eca7a0e0dddcc9e6cdefe0bdcdb
Ingo Nolden <dev@nolden.org>
2019-05-15 22:37:37 +0200

    Added SACN example

---
+++ sacn-example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sacn
+import time
+
+sender = sacn.sACNsender()  # provide an IP-Address to bind to if you are using Windows and want to use multicast
+sender.start()  # start the sending thread
+sender.activate_output(1)  # start sending out data in the 1st universe
+sender[1].multicast = True  # set multicast to True
+# sender[1].destination = "192.168.1.20"  # or provide unicast information.
+# Keep in mind that if multicast is on, unicast is not used
+sender[1].dmx_data = (1, 2, 3, 4)  # some test DMX data
+
+time.sleep(60)  # send the data for 60 seconds
+sender.stop()  # do not forget to stop the sender
